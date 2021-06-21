5e343288b63aa7c41a8bfebceccc67340288e7e6
lijunzhong110 <airsind@gmail.com>
2021-06-21 11:19:29 +1000

    Profile Schema & Get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../modules/Profile');
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
 // Public ---> if you need a token to access a specific route like to add a profile, obviously you need to be authenticated. so you need to send along a token to that route in order for it to work. Otherwise may get 'unauthorized'  And this is a public route which we dont need a token for
-router.get('/', (req, res) => res.send('Profile route'));
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.masssage);
+  }
+});
 
 module.exports = router;
