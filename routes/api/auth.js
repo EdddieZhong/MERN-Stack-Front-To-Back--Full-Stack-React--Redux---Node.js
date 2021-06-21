50f50b0a68ffdc4e54ec8b2878835281ac51421d
lijunzhong110 <airsind@gmail.com>
2021-06-21 21:01:21 +1000

    Revert "Skip to Frontend"
    This reverts commit 08fe6abbf342ab7b90efad0f4814eb90aca6007a.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,32 +2,38 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+
 const jwt = require('jsonwebtoken');
 const config = require('config');
+// const { check, validationResult } = require('express-validator/check');
 const { check, validationResult } = require('express-validator');
 
-const User = require('../../models/User');
+const User = require('../../modules/User');
 
-// @route    GET api/auth
-// @desc     Get user by token
-// @access   Private
+// @route   GET api/auth
+// @desc    Test route
+// @access  Public
+// Public ---> if you need a token to access a specific route like to add a profile, obviously you need to be authenticated. so you need to send along a token to that route in order for it to work. Otherwise may get 'unauthorized'  And this is a public route which we dont need a token for
 router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
     res.json(user);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send('Server error');
   }
 });
 
-// @route    POST api/auth
-// @desc     Authenticate user & get token
-// @access   Public
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+// Public ---> if you need a token to access a specific route like to add a profile, obviously you need to be authenticated. so you need to send along a token to that route in order for it to work. Otherwise may get 'unauthorized'  And this is a public route which we dont need a token for
 router.post(
   '/',
-  check('email', 'Please include a valid email').isEmail(),
-  check('password', 'Password is required').exists(),
+  [
+    check('email', 'Please include a vilid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -39,12 +45,14 @@ router.post(
     try {
       let user = await User.findOne({ email });
 
+      // See if user not exist
       if (!user) {
         return res
           .status(400)
           .json({ errors: [{ msg: 'Invalid Credentials' }] });
       }
 
+      // verify password
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
@@ -55,14 +63,14 @@ router.post(
 
       const payload = {
         user: {
-          id: user.id
-        }
+          id: user.id,
+        },
       };
 
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        { expiresIn: '5 days' },
+        { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path');
 
 const app = express();
 
@@ -8,23 +7,17 @@ const app = express();
 connectDB();
 
 // Init Middleware
+//    used to use app.use(bodyParser.json()) , we can now just use experess.json() should allow us to get the data req.body from user.js
 app.use(express.json());
 
+app.get('/', (req, res) => res.send('API Running'));
+
 // Define Routes
+//  make 'api/users' pertain to '/' in user.8
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 const PORT = process.env.PORT || 5000;
 
