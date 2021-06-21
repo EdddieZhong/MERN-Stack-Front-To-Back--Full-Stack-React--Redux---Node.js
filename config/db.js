08fe6abbf342ab7b90efad0f4814eb90aca6007a
lijunzhong110 <airsind@gmail.com>
2021-06-21 20:43:59 +1000

    Skip to Frontend

--- config/db.js
+++ config/db.js
@@ -1,33 +1,22 @@
-// here lets bringing mongoose, use it to connect
 const mongoose = require('mongoose');
-// and we want to be able to grab that string that we jsut put inside the default.json, so we need also to bring a config package
 const config = require('config');
-// to get that value, call config.get and we can get any of the values in that json file
 const db = config.get('mongoURI');
 
-// to connect to mongo db
-// this will give us a promise, we could use the .then .catch syntax, but we will usee async-await because this is new standard and cleaner, and it's makes your codes lookes like synchronous even though it's asynchronous, so we will be using async-await.
-
-mongoose.connect(db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-// we will use a arrow function since we nedd sth to call within our server.js
 const connectDB = async () => {
-  // in here we want to run like I just showed you mongoose.connect but we want to put it inside a try-catch block, the reason for that is if there are some kinds of error, lick if we can not connect, then we need a way to basically have a fail, and show the error message whatever it is
-  try {
-    // inside the try, since mongoose.connect return a promise, we want to put a await right before , we want to await for that.
-    await mongoose.connect(db);
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true
+		});
 
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    // when sth wrong we just consloe log the error its message property and we want to just escape from the process with failure
-    console.error(err.message);
-    // Exit process with failure
-    process.exit(1);
-  }
+		console.log('MongoDB Connected...');
+	} catch (err) {
+		console.error(err.message);
+		// Exit process with failure
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const router = express.Router();
-//  bring in the gravatar package
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-// const { check, validationResult } = require('express-validator/check');
 const { check, validationResult } = require('express-validator');
+const normalize = require('normalize-url');
 
-const User = require('../../modules/User');
+const User = require('../../models/User');
 
-// @route   POST api/users
-// @desc    Register User
-// @access  Public
-// Public ---> if you need a token to access a specific route like to add a profile, obviously you need to be authenticated. so you need to send along a token to that route in order for it to work. Otherwise may get 'unauthorized'  And this is a public route which we dont need a token for
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
 router.post(
   '/',
-  [
-    check('name', 'Name is required').not().isEmpty(),
-    check('email', 'Please include a vilid email').isEmail(),
-    check(
-      'password',
-      'Please enter a password with 6 or more characters'
-    ).isLength({ min: 6 }),
-  ],
+  check('name', 'Name is required').notEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check(
+    'password',
+    'Please enter a password with 6 or more characters'
+  ).isLength({ min: 6 }),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -35,46 +31,44 @@ router.post(
     try {
       let user = await User.findOne({ email });
 
-      // See if user exists
       if (user) {
         return res
           .status(400)
           .json({ errors: [{ msg: 'User already exists' }] });
       }
 
-      const avatar = gravatar.url(email, {
-        s: '200',
-        r: 'pg',
-        d: 'mm',
-      });
+      const avatar = normalize(
+        gravatar.url(email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm'
+        }),
+        { forceHttps: true }
+      );
 
-      // Get user gravatar
       user = new User({
         name,
         email,
         avatar,
-        password,
+        password
       });
 
-      // Encrypt password
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
 
-      // Return Jsonwebtoken
-
       const payload = {
         user: {
-          id: user.id,
-        },
+          id: user.id
+        }
       };
 
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        { expiresIn: 360000 },
+        { expiresIn: '5 days' },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
